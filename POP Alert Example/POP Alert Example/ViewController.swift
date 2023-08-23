44e167bc8dcc8ffdb60af6c7b1d5beb4285836cd
Matheus Sanada <maatheusdavid@me.com>
2023-08-23 15:24:14 -0300

    init POP Alert

---
+++ POP Alert Example/POP Alert Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POP Alert Example
+//
+//  Created by Matheus Sanada on 23/08/23.
+//
+
+import UIKit
+import POP_Alert
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ POP Alert/POP Alert/POP_Alert.h
@@ -0,0 +1,18 @@
+//
+//  POP_Alert.h
+//  POP Alert
+//
+//  Created by Matheus Sanada on 23/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for POP_Alert.
+FOUNDATION_EXPORT double POP_AlertVersionNumber;
+
+//! Project version string for POP_Alert.
+FOUNDATION_EXPORT const unsigned char POP_AlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POP_Alert/PublicHeader.h>
+
+
